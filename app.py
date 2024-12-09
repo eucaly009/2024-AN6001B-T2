6a12e79d5a39b52225247c4ead9f65c00246335c
eucaly009 <WHE009@e.ntu.edu.sg>
2024-12-09 12:05:57 +0000

    1

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask
-from flask import render_template,request
+from flask import Flask, render_template, request
+import textblob
 
 app = Flask("__name__")
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     return(render_template("index.html"))
 
-@app.route("/main",methods=["GET","POST"])
+@app.route("/main", methods=["GET", "POST"])
 def main():
-    name=request.form.get("q")
+    name = request.form.get("q")
     return(render_template("main.html"))
+
+@app.route("/SA", methods=["GET", "POST"])
+def SA():
+    return(render_template("SA.html"))
+
+@app.route("/SA_result", methods=["POST"])  # Changed to a unique path for the second route
+def SA_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return(render_template("SA_result.html", r=r))
 
 if __name__ == "__main__":
     app.run()
+
+
 
