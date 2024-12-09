4532b101eb8bf8774329996651ab2e240bd0ce88
eucaly009 <WHE009@e.ntu.edu.sg>
2024-12-09 12:46:09 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
+import google.generativeai as genai
 import textblob
+import os
+
+#api="AIzaSyBBRQIIp5vBaOzo4VKtO0qkJwghfSiy90w"
+api= os.getenv("makersuite")
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask("__name__")
 
@@ -16,11 +23,21 @@ def main():
 def SA():
     return(render_template("SA.html"))
 
-@app.route("/SA_result", methods=["POST"])  # Changed to a unique path for the second route
+@app.route("/genAI", methods=["GET", "POST"])
+def genAI():
+    return(render_template("genAI.html"))
+
+@app.route("/SA_result", methods=["POST"])  
 def SA_result():
     q = request.form.get("q")
     r = textblob.TextBlob(q).sentiment
     return(render_template("SA_result.html", r=r))
+
+@app.route("/genAI_result", methods=["POST"])  
+def genAI_result():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("genAI_result.html", r=r.candidates[0].content.parts[0].text))
 
 if __name__ == "__main__":
     app.run()
